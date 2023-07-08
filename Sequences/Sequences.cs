2c61dc2fd3bb735fc86fbac5fc975b9de04b2ee0
Dmitry Nesterenko <trapwhiteclown@gmail.com>
2023-07-08 14:59:39 +0300

    Seq done!

--- Sequences/Sequences.cs
+++ Sequences/Sequences.cs
@@ -21,7 +21,39 @@ namespace Sequences
         /// </exception>
         public static string[] GetSubstrings(string numbers, int length)
         {
-            throw new NotImplementedException("You need to implement this method.");
+            if (string.IsNullOrEmpty(numbers) || string.IsNullOrWhiteSpace(numbers))
+            {
+                throw new ArgumentException("Source string is null or empty or white space.");
+            }
+
+            if (length <= 0)
+            {
+                throw new ArgumentException("Length of substring less or equals zero.");
+            }
+
+            if (length > numbers.Length)
+            {
+                throw new ArgumentException("Length more than length of source string.");
+            }
+
+            List<string> array = new List<string>();
+            for (int i = 0; i <= numbers.Length - length; i++)
+            {
+                string temp = string.Empty;
+                for (int j = i; j < length + i; j++)
+                {
+                    if (numbers[j] != '0' && numbers[j] != '1' && numbers[j] != '2' && numbers[j] != '3' && numbers[j] != '4' && numbers[j] != '5' && numbers[j] != '6' && numbers[j] != '7' && numbers[j] != '8' && numbers[j] != '9')
+                    {
+                        throw new ArgumentException("Source string containing a non-digit character.");
+                    }
+
+                    temp = string.Empty + temp + numbers[j];
+                }
+
+                array.Add(temp);
+            }
+
+            return array.ToArray();
         }
     }
 }
